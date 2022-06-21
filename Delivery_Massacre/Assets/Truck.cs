25c940b4beb0166092a7055d64e99dfdb899d9e5
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-06-21 12:48:27 +0200

    First commit

---
+++ Delivery_Massacre/Assets/Truck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truck : MonoBehaviour
+{
+
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float steerDirection = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        if (steerDirection > 0f)
+        {
+            steerAmount = steerAmount * (-1);
+        }
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, steerDirection, 0);
+
+    }
+}

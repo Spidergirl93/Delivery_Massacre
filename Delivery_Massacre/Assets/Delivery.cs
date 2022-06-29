e7b845683204bfe51063baa6d3dfa5c96a91ede3
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-06-29 13:37:23 +0200

    Change the dolor of the vehicle after picking up and delivering the goods

--- Delivery_Massacre/Assets/Delivery.cs
+++ Delivery_Massacre/Assets/Delivery.cs
@@ -8,6 +8,14 @@ public class Delivery : MonoBehaviour
     bool hasPackege;
     bool isLoaded;
 
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Hello, I collided!");
     }
@@ -18,6 +26,7 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package has been picked up.");
             hasPackege = true;
             isLoaded = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, 1f);
         }
 
@@ -25,6 +34,7 @@ public class Delivery : MonoBehaviour
             Debug.Log("Delivery successful");
             hasPackege = false;
             isLoaded = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
